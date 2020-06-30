4ea00e4966d6018d8403818eb5db24e9d1febddd
Marion Armengaud <armengaudmarion@gmail.com>
2020-06-30 09:56:46 +0200

    models

--- src/model/Project.ts
+++ src/model/Project.ts
@@ -1,14 +1,20 @@
 import {TicketStats} from './ticket-stats';
+import {TicketModel} from './TicketModel';
 
 export class Project {
   name: string;
   admin: string;
-  // @TODO set Ticket model
-  tickets: any[];
+  tickets: TicketModel[];
   ticketStats: TicketStats;
+  isAdmin: boolean;
+  date: Date;
 
   constructor(data?: any) {
     this.name = data.name;
-    this.admin = this.admin;
+    this.admin = data.admin;
+    this.tickets = data.tickets;
+    this.ticketStats = data.ticketStats;
+    this.isAdmin = data.isAdmin;
+    this.date = data.date;
   }
 }
--- src/model/ticket-stats.ts
+++ src/model/ticket-stats.ts
@@ -1,11 +1,13 @@
 export class TicketStats {
   open: number;
-  close: number;
+  resolved: number;
   total: number;
+  inProgress: number;
 
   constructor(data?: any) {
-    this.open = data.Open;
-    this.close = data.close;
+    this.open = data.open;
+    this.resolved = data.resolved;
     this.total = data.total;
+    this.inProgress = data['in progress'];
   }
 }
